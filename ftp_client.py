ce077ad4d4d294a79f0fb9ab4411e6e67258a6f1
Conor <conor4508@gmail.com>
2020-08-28 17:20:59 -0400

    init repo

---
+++ ftp_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import socket
+import tqdm
+import os
+from e_file import FileCrypt
+
+
+SEPERATOR = "<SEPERATOR>"
+BUFF_SIZE = 4096
+
+sock = socket.socket()
+sock.bind(("localhost", 6069))
+sock.listen(5)
+
+client_socket, addr = sock.accept()
+
+recv = client_socket.recv(BUFF_SIZE).decode()
+
+key, name, size = recv.split(SEPERATOR)
+
+name = os.path.basename(name)
+
+size = int(size)
+
+print("Received: ", key, name, size)
+
+progress = tqdm.tqdm(range(size), f"Receiving {name}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(name, "wb") as f:
+    for _ in progress:
+        bytes_read = client_socket.recv(BUFF_SIZE)
+        if not bytes_read:    
+            break
+        f.write(bytes_read)
+        progress.update(len(bytes_read))
+
+    client_socket.close()
+    sock.close()
---
+++ ftp_server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import socket
+import tqdm
+import os
+import sys
+import re
+from e_file import FileCrypt
+
+
+class E_FTP_S:
+    def __init__(self):
+        self.addr = self.get_addr()
+        self.port = 6069
+        self.filename = input("Please Enter Filename:\n")
+        self.buff_size = 4096
+        self.seperator = "<SEPERATOR>"
+        self.e_file = FileCrypt()
+        self.key = self.e_file.key()
+        if not self.validate_file():
+            print("No such file exists")
+            sys.exit()
+        self.file_size = os.path.getsize(self.filename)
+        
+        self.connect()
+
+    def get_addr(self):
+        pattern = '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
+        addr = input("Please Enter Destination Address:\n")
+        if addr.lower() == "localhost":
+            return "localhost"
+        if re.match(pattern, addr):
+            return addr
+        else:
+            print("Invalid Address")
+            sys.exit()
+
+    def validate_file(self):
+        return os.path.exists(self.filename)
+
+
+    def connect(self):
+        sock = socket.socket()
+        sock.connect((self.addr, self.port))
+        sock.send(f"{self.key}{self.seperator}{self.filename}{self.seperator}{self.file_size}".encode())
+        progress = tqdm.tqdm(range(self.file_size), f"Sending {self.filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        self.e_file.encrypt_file(self.filename)
+        with open(self.filename, "rb") as f:
+            for _ in progress:
+                bytes_read = f.read(self.buff_size)
+                if not bytes_read:
+                    break
+                sock.sendall(bytes_read)
+                progress.update(len(bytes_read))
+        sock.close()
+
+server_obj = E_FTP_S()
