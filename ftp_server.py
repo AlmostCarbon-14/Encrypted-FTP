1022058780277711c4695e6d6736bb33c4296bf9
Conor <conor4508@gmail.com>
2020-08-30 15:13:15 -0400

    finding host IP

--- ftp_server.py
+++ ftp_server.py
@@ -4,8 +4,22 @@ import tqdm
 import os
 import time
 import sys
-import subprocess as sp
 from e_file import FileCrypt
+
+
+def get_ip_addr():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = ""
+    try:
+        sock.connect(("8.8.8.8", 80))
+        addr = sock.getsockname()[0]
+        print("ADDRESS: ", addr)
+        sock.close()
+    except:
+        print("Ouch")
+    return addr
+        
+
 
 
 SEPERATOR = "<SEPERATOR>"
@@ -16,18 +30,14 @@ if "-p" in args: #this is for later if I feel like it
 else:
     PORT = input("Please Enter a Port Number: \n")
 
-cmd = "ifconfig | grep 255.255.255.0"
 
-inet = sp.check_output(cmd, shell = True)
-inet = wlan.decode("utf-8")
-inet = wlan.split(" ")
-inet_addr = inet[inet.index("inet") + 1]
-print(inet_addr)
+ip_addr = get_ip_addr()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.bind((socket.gethostbyname(socket.gethostname()), int(PORT)))
-print(f"Listening at {socket.gethostbyname(socket.gethostname())} : {PORT}")
+sock.bind((ip_addr, int(PORT)))
+
+print(f"Listening at {ip_addr} : {PORT}")
 sock.listen(5)
 
 client_socket, addr = sock.accept()
