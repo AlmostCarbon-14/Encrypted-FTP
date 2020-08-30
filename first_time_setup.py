f807836292d69d0915dd88a9eec788003f17b951
Conor <conor4508@gmail.com>
2020-08-30 14:10:46 -0400

    more stuff

--- first_time_setup.py
+++ first_time_setup.py
@@ -16,11 +16,13 @@ except:
         print("There's been an error installing a necessary package for this client to work, attempting to update apt-get in 10 seconds...")
         time.sleep(10)
         os.system("sudo apt-get update -y")
+        print("="*10 + "Update Complete, Now Upgrading apt-get" + "="*10)
+        time.sleep(5)
         os.system("sudo apt-get upgrade -y")
         if flag:
             os.system("sudo apt-get -y install python3-pip")
 
-        if 'error' in sp.check_output(['pip3','install','tqdm']):
+        if b'error' in sp.check_output(['pip3','install','tqdm']):
             print("The error has occured again, please try something else, maybe switching python versions? Or attempt to manually install tqdm via pip")
             sys.exit()
         else:
